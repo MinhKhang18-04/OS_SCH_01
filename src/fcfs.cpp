b23a74ed57b725f08086091ebff299203362c7ec
Lê Hoàng Minh Khang <0937712182khang@gmail.com>
2026-01-17 00:13:52 +0700

    Update fcfs.cpp

--- src/fcfs.cpp
+++ src/fcfs.cpp
@@ -1,61 +1,59 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
 struct Process {
-    string id;
-    int at, bt, ct, wt, tat;
+    string pid;
+    int arrival;
+    int burst;
+    int waiting;
+    int turnaround;
 };
 
 int main() {
-    int n;
-    cout << "Nhap so luong tien trinh: ";
-    cin >> n;
-
-    vector<Process> p(n);
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap ID, Arrival Time, Burst Time cho P" << i + 1 << ": ";
-        cin >> p[i].id >> p[i].at >> p[i].bt;
+    ifstream file("input.csv");
+    if (!file) {
+        cout << "Cannot open input.csv\n";
+        return 1;
     }
 
-    // Sap xep theo thoi gian den (Arrival Time)
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (p[i].at > p[j].at) {
-                swap(p[i], p[j]);
-            }
-        }
+    vector<Process> p;
+    string line;
+
+    getline(file, line); // skip header
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Process pr;
+        string temp;
+
+        getline(ss, pr.pid, ',');
+        getline(ss, temp, ','); pr.arrival = stoi(temp);
+        getline(ss, temp, ','); pr.burst = stoi(temp);
+
+        p.push_back(pr);
     }
 
-    int current_time = 0;
-    float sumWT = 0, sumTAT = 0;
+    int time = 0;
+    for (int i = 0; i < p.size(); i++) {
+        if (time < p[i].arrival)
+            time = p[i].arrival;
 
-    for (int i = 0; i < n; i++) {
-        if (current_time < p[i].at) {
-            current_time = p[i].at;
-        }
-        p[i].ct = current_time + p[i].bt;
-        p[i].tat = p[i].ct - p[i].at;
-        p[i].wt = p[i].tat - p[i].bt;
-        
-        current_time = p[i].ct;
-        
-        sumWT += p[i].wt;
-        sumTAT += p[i].tat;
+        p[i].waiting = time - p[i].arrival;
+        time += p[i].burst;
+        p[i].turnaround = p[i].waiting + p[i].burst;
     }
 
-    // In ket qua ra man hinh
-    cout << "\nKQS: First-Come First-Served\n";
-    cout << "ID\tAT\tBT\tCT\tTAT\tWT\n";
-    for (int i = 0; i < n; i++) {
-        cout << p[i].id << "\t" << p[i].at << "\t" << p[i].bt << "\t" 
-             << p[i].ct << "\t" << p[i].tat << "\t" << p[i].wt << endl;
+    cout << "FCFS Scheduling Result\n";
+    cout << "PID\tArrival\tBurst\tWaiting\tTurnaround\n";
+    for (auto pr : p) {
+        cout << pr.pid << "\t"
+             << pr.arrival << "\t"
+             << pr.burst << "\t"
+             << pr.waiting << "\t"
+             << pr.turnaround << "\n";
     }
-
-    cout << "\nThoi gian cho trung binh: " << sumWT / n;
-    cout << "\nThoi gian hoan thanh trung binh: " << sumTAT / n << endl;
 
     return 0;
 }
